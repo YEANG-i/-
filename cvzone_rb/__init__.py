e645a8e2d90e9ff0e15e87a9601277cb1025c316
YEANG <justyeang@163.com>
2022-04-02 10:41:22 +0800

    创建第三方库

---
+++ cvzone_rb/__init__.py
@@ -0,0 +1,15 @@
+from cvzone_rb.ClassificationModule import Classifier
+from cvzone_rb.ColorModule import ColorFinder
+from cvzone_rb.FaceDetectionModule import FaceDetector
+from cvzone_rb.FaceMeshModule import FaceMeshDetector
+from cvzone_rb.FPS import FPS
+from cvzone_rb.HandTrackingModule import HandDetector
+from cvzone_rb.PIDModule import PID
+from cvzone_rb.PlotModule import LivePlot
+from cvzone_rb.PoseModule import PoseDetector
+from cvzone_rb.SelfiSegmentationModule import SelfiSegmentation
+from cvzone_rb.SerialModule import SerialObject
+from cvzone_rb.Utils import stackImages, cornerRect, findContours, \
+    overlayPNG, rotateImage, putTextRect
+
+
